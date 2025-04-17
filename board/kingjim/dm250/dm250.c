7767f1085c6cd37ef2e71007e9f1267fe5565222
joshua stein <jcs@jcs.org>
2025-04-17 11:15:42 -0500

    board: add KING JIM DM250 based on rk3128 soc

---
+++ board/kingjim/dm250/dm250.c
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier:     GPL-2.0+ */
+
+#include <common.h>
+#include <dm.h>
+#include <asm/io.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int mach_cpu_init(void)
+{
+        return 0;
+}
